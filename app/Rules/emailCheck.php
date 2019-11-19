ab1ddb4ebc38adf842a445539ae75b51c0a7d47c
Robert <rsztabralowski@gmail.com>
2019-11-19 20:30:00 +0000

    Added check email rule

---
+++ app/Rules/emailCheck.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class emailCheck implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+
+        $emails = file_get_contents('https://icingprint.co.uk/script/event_email.php');
+
+        $emails_array = json_decode($emails, true);
+
+        if(in_array($value, $emails_array))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return "Please use your Eventbrite email";
+    }
+}

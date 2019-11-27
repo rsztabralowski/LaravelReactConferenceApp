898773938a4caaa74f8247ef97510261accbc30e
Robert <rsztabralowski@gmail.com>
2019-11-27 21:16:58 +0000

    Routes updated by Billy

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/map', 'HomeController@map')->name('map');
+
+Route::get('/events', 'HomeController@events')->name('events');
